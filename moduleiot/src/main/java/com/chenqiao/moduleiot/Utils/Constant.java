eebe0fc8161b51582c53181831439a9459c33f8b
mrjctech <mrjctech@gmail.com>
2019-06-08 23:57:45 +0800

    utils

---
+++ moduleiot/src/main/java/com/chenqiao/moduleiot/Utils/Constant.java
@@ -0,0 +1,12 @@
+package com.chenqiao.moduleiot.Utils;
+
+import android.os.Environment;
+
+/**
+ * Created by chenqiao on 2019/5/30.
+ * e-mail : mrjctech@gmail.com
+ */
+public class Constant {
+
+    public static final String DISK_FILE = Environment.getExternalStorageDirectory() + "/.aliot/.dsk";
+}
